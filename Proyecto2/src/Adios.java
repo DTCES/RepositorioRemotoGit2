6bdaba0bc133dc97f89a16452e23377f708a47e0
DTCES <danieltabascocastellano@gmail.com>
2026-04-01 21:28:53 +0200

    a

---
+++ Proyecto2/src/Adios.java
@@ -0,0 +1,7 @@
+
+public class Adios {
+	public static void main(String[] args) {
+		System.out.println("Adios");
+	}
+	
+}
---
+++ Proyecto2/src/YoQSe.java
@@ -0,0 +1,7 @@
+
+public class YoQSe {
+	public static void main(String[] args) {
+		System.out.println("I dunno");
+	}
+	
+}
